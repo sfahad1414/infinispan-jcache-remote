65e4dd726e1da158a68d772d82ec5f5f5d96c2fd
fshaikh <fshaikh@digite.com>
2022-02-11 20:38:15 +0530

    removed jackson as it tries to trave the whole object in case if it is hibernate using Object Serialization Technique

--- src/main/java/cache/adaptor/MapAdaptor.java
+++ src/main/java/cache/adaptor/MapAdaptor.java
@@ -1,27 +1,34 @@
 package cache.adaptor;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.infinispan.protostream.annotations.ProtoAdapter;
 import org.infinispan.protostream.annotations.ProtoFactory;
 import org.infinispan.protostream.annotations.ProtoField;
 
+import java.io.*;
 import java.util.HashMap;
 
 @ProtoAdapter(HashMap.class)
 public class MapAdaptor
 {
-    ObjectMapper objectMapper = new ObjectMapper();
-
     @ProtoFactory
-    HashMap<?, ?> create(String map) throws JsonProcessingException {
-        return objectMapper.readValue(map, new TypeReference<>() {
-        });
+    HashMap<?, ?> create(byte[] map) throws IOException {
+        ByteArrayInputStream bis = null;
+        ObjectInputStream ois = null;
+        try {
+            bis = new ByteArrayInputStream(map);
+            ois = new ObjectInputStream(bis);
+            return (HashMap<?,?>) ois.readObject();
+        }catch (ClassNotFoundException e){
+            throw new IOException(e.getMessage());
+        }
+
     }
 
     @ProtoField(1)
-    String getMap(HashMap<?, ?> data) throws JsonProcessingException {
-        return objectMapper.writeValueAsString(data);
+    byte[] getMap(HashMap<?, ?> data) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(data);
+        return bos.toByteArray();
     }
 }
